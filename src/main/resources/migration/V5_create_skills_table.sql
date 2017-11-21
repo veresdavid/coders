a5f435b736531d58299dd3e4d72851f51cf20464
BeardOfDoom <vecsi96@gmail.com>
2017-11-21 12:13:02 +0100

    Script for creating skills table

---
+++ src/main/resources/migration/V5_create_skills_table.sql
@@ -0,0 +1,16 @@
+-- SKILLS TABLE
+
+CREATE TYPE skill_types AS ENUM ('offensive', 'defensive', 'adaptive', 'basic');
+
+CREATE TABLE public.skills
+(
+    id bigint NOT NULL DEFAULT nextval('hibernate_sequence'),
+    name character varying COLLATE pg_catalog."default",
+	type skill_types,
+	prerequisites character varying COLLATE pg_catalog."default",
+	CONSTRAINT skill_pkey PRIMARY KEY (id)
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
