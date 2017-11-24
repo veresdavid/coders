84904dc2f560e913512dccc5801ba2462851468c
Veres Dávid <veres.david11@gmail.com>
2017-11-24 13:03:36 +0100

    Added null checks to job and level services

--- src/main/java/hu/unideb/inf/coders/service/JobServiceImpl.java
+++ src/main/java/hu/unideb/inf/coders/service/JobServiceImpl.java
@@ -1,6 +1,7 @@
 package hu.unideb.inf.coders.service;
 
 import hu.unideb.inf.coders.dto.JobDTO;
+import hu.unideb.inf.coders.entity.JobEntity;
 import hu.unideb.inf.coders.repository.JobRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,20 +10,30 @@ import org.springframework.stereotype.Service;
 @Service
 public class JobServiceImpl implements JobService {
 
-    @Autowired
-    private JobRepository jobRepository;
+	@Autowired
+	private JobRepository jobRepository;
 
-    @Autowired
-    private ModelMapper modelMapper;
+	@Autowired
+	private ModelMapper modelMapper;
 
-    @Override
-    public JobDTO findById(Long id) {
-        return modelMapper.map(jobRepository.findById(id), JobDTO.class);
-    }
+	@Override
+	public JobDTO findById(Long id) {
 
-    @Override
-    public JobDTO findByName(String name) {
-        return modelMapper.map(jobRepository.findByName(name), JobDTO.class);
-    }
+		JobEntity jobEntity = jobRepository.findById(id);
+
+		if (jobEntity == null) return null;
+
+		return modelMapper.map(jobEntity, JobDTO.class);
+	}
+
+	@Override
+	public JobDTO findByName(String name) {
+
+		JobEntity jobEntity = jobRepository.findByName(name);
+
+		if (jobEntity == null) return null;
+
+		return modelMapper.map(jobEntity, JobDTO.class);
+	}
 
 }
