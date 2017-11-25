4250abcf23d23e80b40e5e9a3a58f667b27cc3b5
BeardOfDoom <vecsi96@gmail.com>
2017-11-25 10:11:56 +0100

    Added null check to skill service

--- src/main/java/hu/unideb/inf/coders/service/SkillServiceImpl.java
+++ src/main/java/hu/unideb/inf/coders/service/SkillServiceImpl.java
@@ -1,6 +1,7 @@
 package hu.unideb.inf.coders.service;
 
 import hu.unideb.inf.coders.dto.SkillDTO;
+import hu.unideb.inf.coders.entity.SkillEntity;
 import hu.unideb.inf.coders.repository.SkillRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,12 +18,24 @@ public class SkillServiceImpl implements SkillService {
 
     @Override
     public SkillDTO findById(Long id) {
-        return modelMapper.map(skillRepository.findById(id), SkillDTO.class);
+
+        SkillEntity skillEntity = skillRepository.findById(id);
+
+        if (skillEntity == null) return null;
+
+        return modelMapper.map(skillEntity, SkillDTO.class);
+
     }
 
     @Override
     public SkillDTO findByName(String name) {
-        return modelMapper.map(skillRepository.findByName(name), SkillDTO.class);
+
+        SkillEntity skillEntity = skillRepository.findByName(name);
+
+        if (skillEntity == null) return null;
+
+        return modelMapper.map(skillEntity, SkillDTO.class);
+
     }
 
 }
