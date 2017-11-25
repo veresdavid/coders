39c6723da6f6d91f390ebda5ab08e4d20bfac855
BeardOfDoom <vecsi96@gmail.com>
2017-11-25 14:39:32 +0100

    Added insert skills script for test purposes

---
+++ src/main/resources/migration/V7__insert_skills.sql
@@ -0,0 +1,20 @@
+INSERT INTO skills (id, name, type, prerequisites)
+VALUES (0, 'Programming', 'BASIC', '');
+INSERT INTO skills (id, name, type, prerequisites)
+VALUES (1, 'Test1', 'BASIC', '0');
+INSERT INTO skills (id, name, type, prerequisites)
+VALUES (2, 'Test2', 'OFFENSIVE', '1');
+INSERT INTO skills (id, name, type, prerequisites)
+VALUES (3, 'Test3', 'DEFENSIVE', '0');
+INSERT INTO skills (id, name, type, prerequisites)
+VALUES (4, 'Test4', 'OFFENSIVE', '2');
+INSERT INTO skills (id, name, type, prerequisites)
+VALUES (5, 'Test5', 'ADAPTIVE', '1');
+INSERT INTO skills (id, name, type, prerequisites)
+VALUES (6, 'Test6', 'DEFENSIVE', '3');
+INSERT INTO skills (id, name, type, prerequisites)
+VALUES (7, 'Test7', 'BASIC', '1');
+INSERT INTO skills (id, name, type, prerequisites)
+VALUES (8, 'Test8', 'DEFENSIVE', '1');
+INSERT INTO skills (id, name, type, prerequisites)
+VALUES (9, 'Test9', 'ADAPTIVE', '4');
