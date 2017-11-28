1e833bb06e00b11851288059153b3b45bb4c1c03
BeardOfDoom <vecsi96@gmail.com>
2017-11-28 17:57:15 +0100

    System message when attack finished

--- src/main/java/hu/unideb/inf/coders/controller/rest/AttackRestController.java
+++ src/main/java/hu/unideb/inf/coders/controller/rest/AttackRestController.java
@@ -1,18 +1,18 @@
 package hu.unideb.inf.coders.controller.rest;
 
+import hu.unideb.inf.coders.dto.MessageDTO;
 import hu.unideb.inf.coders.dto.UserAttackDTO;
 import hu.unideb.inf.coders.dto.UserDTO;
 import hu.unideb.inf.coders.response.ActiveUserAttackResponse;
 import hu.unideb.inf.coders.response.FinishedUserAttackResponse;
-import hu.unideb.inf.coders.service.AuthenticationFacade;
-import hu.unideb.inf.coders.service.UserAttackManager;
-import hu.unideb.inf.coders.service.UserAttackService;
-import hu.unideb.inf.coders.service.UserService;
+import hu.unideb.inf.coders.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping(path = "/attack")
@@ -29,6 +29,9 @@ public class AttackRestController {
 
     @Autowired
     private AuthenticationFacade authenticationFacade;
+
+    @Autowired
+    private MessageManager messageManager;
 
     @RequestMapping(path = "/start/{defenderId}", method = RequestMethod.POST)
     public ActiveUserAttackResponse startAttack(@PathVariable(name = "defenderId") Long defenderId) {
@@ -75,7 +78,9 @@ public class AttackRestController {
 
         boolean success = attackerUserDTO.getId() == winnerUserDTO.getId();
 
-        return new FinishedUserAttackResponse(defenderUserDTO.getName(), success?100:0, success?50:0, success);
+        sendSystemMessages(attackerUserDTO, defenderUserDTO, success);
+
+        return new FinishedUserAttackResponse(defenderUserDTO.getName(), success?userAttackManager.XP_GAIN:0, success?userAttackManager.MONEY_GAIN:0, success);
 
     }
 
@@ -98,6 +103,33 @@ public class AttackRestController {
 
         return new ActiveUserAttackResponse(defenderUserDTO.getName(), userAttackDTO.getFinish());
 
+    }
+
+    private void sendSystemMessages(UserDTO attackerUserDTO, UserDTO defenderUserDTO, boolean success) {
+
+        String subjectAttacker = "Attack has finished!";
+        String messageAttacker;
+        String subjectDefender = "You have been attacked!";
+        String messageDefender;
+
+        if(success) {
+            messageAttacker = "The attack was successful!\n" +
+                    "You have gained " + userAttackManager.XP_GAIN + " XP and " + userAttackManager.MONEY_GAIN + " money.";
+            messageDefender = "The defend was unsuccessful!\n" +
+                    "You have gained 0 XP and 0 money.";
+        } else {
+            messageAttacker = "The attack was unsuccessful!\n" +
+                    "You have gained 0 XP and 0 money.";
+            messageDefender = "The defend was successful!\n" +
+                    "You have gained " + userAttackManager.XP_GAIN + " XP and " + userAttackManager.MONEY_GAIN + " money.";
+
+        }
+        MessageDTO attackerMessageDTO = new MessageDTO(null, null, attackerUserDTO.getId(), "SYSTEM", subjectAttacker, messageAttacker, LocalDateTime.now(), false);
+        MessageDTO defenderMessageDTO = new MessageDTO(null, null, defenderUserDTO.getId(), "SYSTEM", subjectDefender, messageDefender, LocalDateTime.now(), false);
+
+
+        messageManager.sendMessage(attackerMessageDTO);
+        messageManager.sendMessage(defenderMessageDTO);
     }
 
 }
