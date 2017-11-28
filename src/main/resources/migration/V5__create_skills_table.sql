ba15e241894edaa6f5ca800c8a2f447c980b70a3
Veres Dávid <veres.david11@gmail.com>
2017-11-28 16:04:30 +0100

    Removed enums from SQL scripts

--- src/main/resources/migration/V5__create_skills_table.sql
+++ src/main/resources/migration/V5__create_skills_table.sql
@@ -1,12 +1,10 @@
 -- SKILLS TABLE
 
-CREATE TYPE skill_types AS ENUM ('OFFENSIVE', 'DEFENSIVE', 'ADAPTIVE', 'BASIC');
-
 CREATE TABLE public.skills
 (
     id bigint NOT NULL DEFAULT nextval('hibernate_sequence'),
     name character varying COLLATE pg_catalog."default",
-	type skill_types,
+	type character varying COLLATE pg_catalog."default",
 	prerequisites character varying COLLATE pg_catalog."default",
 	CONSTRAINT skill_pkey PRIMARY KEY (id)
 )
--- src/main/resources/migration/V9__create_messages_table.sql
+++ src/main/resources/migration/V9__create_messages_table.sql
@@ -1,13 +1,13 @@
 -- MESSAGES TABLE
 
-CREATE TYPE message_types AS ENUM ('SYSTEM', 'USER');
+-- CREATE TYPE message_types AS ENUM ('SYSTEM', 'USER');
 
 CREATE TABLE public.messages
 (
     id bigint NOT NULL DEFAULT nextval('hibernate_sequence'),
     sender_id bigint,
     receiver_id bigint,
-    type message_types,
+    type character varying COLLATE pg_catalog."default",
     subject character varying COLLATE pg_catalog."default",
     message character varying COLLATE pg_catalog."default",
     date timestamp,
